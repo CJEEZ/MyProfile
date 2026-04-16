7caddb0a4fd728c6ab1f8062a18a06f87896cd2a
CieJeez <clarencejohnmarquez2@Gmail.com>
2026-04-17 04:29:03 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function showAlert() {
+    alert("Thank you for your interest in our farm! We will connect you to our produce list shortly.");
+}
+
+// Log a message to the console to confirm the script is running
+console.log("Farm Profile Web Server is active.");
